276f409911a397adf84bbdf99a431c74a5d993a6
Yusun <solarpy@hanyang.ac.kr>
2022-06-28 01:33:00 +0900

    Types of TS part Three

--- Types.ts
+++ Types.ts
@@ -35,12 +35,48 @@ player2[1] = 3; // readonly는 수정 불가함
 //
 let b: undefined = undefined;
 let c: null = null;
-let d: any = 1;     // 어떤 것이든 가능
-
+let d: any = 1; // 어떤 것이든 가능
 
 // any를 써서 어떤 것이든 가능하게 했다.
-const e : any[] = [1,2,3,4]
-e.push("asdf")
+const e: any[] = [1, 2, 3, 4];
+e.push("asdf");
 
-e+d // any + any 이기에 에러가 안남
+e + d; // any + any 이기에 에러가 안남
 
+// unknown 타입은 사전에 확인하는 코드가 필요하다.
+// (맞는지 안맞는지 확인 후 진행하게 된다.)
+let f: unknown;
+
+if (typeof f === "number") {
+  let g = f + 1;
+}
+
+if (typeof f === "string") {
+  let g = f + 1;
+}
+
+// function 도 return 을 강제하려면 타입을 지정해준다.
+function hello(): string {
+  console.log("x");
+  return "a";
+}
+
+// never 타입 : 절대로 return 을 하면 안된다.
+function hello_never(): never {
+  throw new Error("xxx");
+
+}
+
+// input 타입이 아닌 것도 never
+function never(name:string | number) {
+    if(typeof name === 'string'){
+        name // 이건 타입이 string
+    } 
+    else if (typeof name === 'number') {
+        name // 이건 타입이 number
+    }
+    else {
+        name // 이건 타입이 never
+    }
+  
+  }
